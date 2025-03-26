1400edc454c7332f413786d5efd285b27dfd89ef
SauravAcharya23 <sauravacharya236@gmail.com>
2025-03-26 19:36:42 +0545

    Firebase initialization to the database is done

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,22 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
+
+}
+dependencies {
+  // Import the Firebase BoM
+  implementation(platform("com.google.firebase:firebase-bom:33.11.0"))
+
+
+  // TODO: Add the dependencies for Firebase products you want to use
+  // When using the BoM, don't specify versions in Firebase dependencies
+  implementation("com.google.firebase:firebase-analytics")
+
+
+  // Add the dependencies for any other desired Firebase products
+  // https://firebase.google.com/docs/android/setup#available-libraries
 }
 
 android {
@@ -24,7 +40,7 @@ android {
         applicationId = "com.example.car_rental_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21 //flutter.minSdkVersion
+        minSdk = 23 //flutter.minSdkVersion
         targetSdk = 34 //flutter.targetSdkVersion
         versionCode = 1 //flutter.versionCode
         versionName = "1.0" //flutter.versionName
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
-import 'package:car_rental_app/presentation/view/pages/Bottom_Navbar_page/bottom_navbar.dart';
+import 'package:car_rental_app/presentation/view/pages/Login_Page/login.dart';
 import 'package:car_rental_app/routes/app_routes.dart';
 import 'package:car_rental_app/routes/routes.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   // Device Preview Code
   // runApp(
   //   DevicePreview(
@@ -29,8 +32,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       routes: AppRoutes.routes,
-      initialRoute: Routes.bottomnavbar,
-      home: const BottomNavBar()
+      initialRoute: Routes.login,
+      home: const LoginPage()
     );
   }
 }
